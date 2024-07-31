628f60a4e546e4c669bc35d7bb8900dcb5dc984b
Nandhakumar <nandhakumar-b@lntecc.com>
2024-07-31 18:11:34 +0530

    commited templete driven form changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-app';
+
+  onSubmit(f:NgForm){
+    console.log("f:", f.value)
+  }
+
+  getValue(fullName:NgModel){
+    console.log("fullName:", fullName)
+  }
 }
